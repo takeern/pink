eee3967de6475a8c360930b1eff4a6bfee1e5e1e
tangqi <tangqi@bilibili.com>
2018-12-06 15:36:02 +0800

    [update] delete responsbility

--- config/webpack.server.js
+++ config/webpack.server.js
@@ -4,7 +4,6 @@ const { p } = require('./webpack.baseConfig');
 module.exports = function () {
     return new Promise(resolve => {
         p.then(data => {
-            console.log(data.servConfig);
             resolve(data.servConfig);
         });
     });
