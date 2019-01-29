5798e0b55b403e6c1d345da358f3423a0ab0d9e1
tangqi <tangqi@bilibili.com>
2019-01-29 18:00:04 +0800

    [update] add pdf

--- src/component/Issue.jsx
+++ src/component/Issue.jsx
@@ -3,34 +3,34 @@ import React from 'react';
 export default () => {
     const pathMap = [
         {
-            name: 'Vol 1 NO 6(IJMSR 2018)',
-            path: '',
+            name: 'Vol 1, No 6 (IJMSR 2018)',
+            path: '1-6-2018',
         },
         {
-            name: 'Vol 1 NO 5(IJMSR 2018)',
-            path: '',
+            name: 'Vol 1, NO 5(IJMSR 2018)',
+            path: '1-5-2018',
         },
         {
-            name: 'Vol 1 NO 4(IJMSR 2018)',
-            path: '',
+            name: 'Vol 1, NO 4(IJMSR 2018)',
+            path: '1-4-2018',
         },
         {
-            name: 'Vol 1 NO 3(IJMSR 2018)',
-            path: '',
+            name: 'Vol 1, NO 3(IJMSR 2018)',
+            path: '1-3-2018',
         },
         {
-            name: 'Vol 1 NO 2(IJMSR 2018)',
-            path: '',
+            name: 'Vol 1, NO 2(IJMSR 2018)',
+            path: '1-2-2018',
         },
         {
-            name: 'Vol 1 NO 1(IJMSR 2018)',
-            path: '',
+            name: 'Vol 1, NO 1(IJMSR 2018)',
+            path: '1-1-2018',
         },
     ];
     const list = pathMap.map((item, index) => {
         return (
             <div key={index} className='issues'>
-                <a href={item.path}>{item.name}</a>
+                <a href={`/src/static/img/${item.path}.pdf`}>{item.name}</a>
                 <br />
                 <br />
             </div>
